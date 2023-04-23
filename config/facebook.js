2fc2305a7100cd413d1f19c1e8d6e1691090689a
pranavkaleweb <kale7281@gmail.com>
2023-04-23 18:55:21 +0530

    changes

--- config/facebook.js
+++ config/facebook.js
@@ -1,4 +1,4 @@
-const obj = {
+const fbLogObj = {
     clientID: 1003935064107659,
     secret: '9715788c84a8402b01c8b2a448754325',
     redirectUrl: 'login.html',
---
+++ utils/facebookHelper.js
@@ -0,0 +1,30 @@
+// This is called with the results from from FB.getLoginStatus().
+let fbHelper = {
+    statusChangeCallback : function(response) {
+    console.log('statusChangeCallback');
+    console.log(response);
+    // The response object is returned with a status field that lets the
+    // app know the current login status of the person.
+    // Full docs on the response object can be found in the documentation
+    // for FB.getLoginStatus().
+    if (response.status === 'connected') {
+    // Logged into your app and Facebook.
+    testAPI();
+    } else if (response.status === 'not_authorized') {
+    // The person is logged into Facebook, but not your app.
+    document.getElementById('status').innerHTML = 'Login with Facebook ';
+    } else {
+    // The person is not logged into Facebook, so we're not sure if
+    // they are logged into this app or not.
+    document.getElementById('status').innerHTML = 'Login with Facebook ';
+    }
+    },
+    // This function is called when someone finishes with the Login
+    // Button. See the onlogin handler attached to it in the sample
+    // code below.
+    checkLoginState : function() {
+    FB.getLoginStatus(function(response) {
+    statusChangeCallback(response);
+    });
+    }
+}
